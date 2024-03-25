4c5f106057d4eee2f837f3cded1b62585db9640e
brunno-toscano <brunno.toscano@ibm.com>
2024-03-25 14:08:29 -0300

    Implementação da validação de campos obrigatórios

--- src/main/java/io/github/brunnotoscano/domain/entity/Cliente.java
+++ src/main/java/io/github/brunnotoscano/domain/entity/Cliente.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.util.Set;
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "cliente")
@@ -20,9 +22,15 @@ public class Cliente {
 
     @Column(name = "id")
     private Integer id;
+
+
     @Column(name = "nome", length = 100)
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String nome;
+
     @Column(name = "cpf")
+    @NotEmpty(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.cpf.invalido}")
     private String cpf;
 
     @JsonIgnore
--- src/main/java/io/github/brunnotoscano/rest/dto/PedidoDTO.java
+++ src/main/java/io/github/brunnotoscano/rest/dto/PedidoDTO.java
@@ -13,17 +13,24 @@ package io.github.brunnotoscano.rest.dto;
 
 import io.github.brunnotoscano.domain.entity.Produto;
 import io.github.brunnotoscano.domain.enums.StatusPedido;
+import io.github.brunnotoscano.validation.NotEmptyList;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class PedidoDTO {
+    @NotNull(message = "{campo.codigo-cliente.obrigatorio}")
     private Integer cliente;
+
+    @NotNull(message = "{campo.total-pedido.obrigatorio}")
     private BigDecimal total;
+
+    @NotEmptyList(message = "{campo.items-pedido.obrigatorio}")
     private List<ItemPedidoDTO> itens;
 }
