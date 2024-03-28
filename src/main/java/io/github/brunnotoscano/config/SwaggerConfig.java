93bbda75536eed937bed1a213c5be41878ef0a57
brunno-toscano <brunno.toscano@ibm.com>
2024-03-28 11:07:24 -0300

    implementação inicial swagger

--- src/main/java/io/github/brunnotoscano/config/SwaggerConfig.java
+++ src/main/java/io/github/brunnotoscano/config/SwaggerConfig.java
@@ -1,2 +1,25 @@
-package io.github.brunnotoscano.config;public class SwaggerConfig {
+package io.github.brunnotoscano.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket(){
+
+    }
+
+    private Contact contact(){
+        return new Contact("Brunno Toscano Messias",
+                "https://github.com/brunnotm23",
+                "brunnotm23@gmail.com");
+    }
+
+
 }
--- src/main/java/io/github/brunnotoscano/rest/dto/PedidoDTO.java
+++ src/main/java/io/github/brunnotoscano/rest/dto/PedidoDTO.java
@@ -1,18 +1,5 @@
 package io.github.brunnotoscano.rest.dto;
 
-//{
-//    "cliente" : 1,
-//    "total" : 100,
-//    "itens" : [
-//        {
-//            "produto" : 1,
-//            "quantidade" : 1
-//        }
-//    ]
-//}
-
-import io.github.brunnotoscano.domain.entity.Produto;
-import io.github.brunnotoscano.domain.enums.StatusPedido;
 import io.github.brunnotoscano.validation.NotEmptyList;
 import lombok.AllArgsConstructor;
 import lombok.Data;
